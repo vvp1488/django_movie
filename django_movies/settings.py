c6551537ac9a41685cf84c6a5b734081f4a56616
vvp1488 <drozdik24011994@gmail.com>
2022-05-28 19:36:48 +0300

    s

--- django_movies/settings.py
+++ django_movies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'djangomovies1.herokuapp.com']
 
